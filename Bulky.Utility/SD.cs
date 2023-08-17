3d312cca514a861a76f452a42cd206ed4445a3de
z1cLa <miljanlazic@gmail.com>
2023-08-17 16:05:43 +0200

    Section 12 finished, bug in last task

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -16,6 +16,7 @@ namespace Bulky.Utility
         public const string StatusPending = "Pending";
         public const string StatusApproved = "Approved";
         public const string StatusInProcess= "Processing";
+        public const string StatusShipped = "Shipped";
         public const string StatusCancelled= "Cancelled";
         public const string StatusRefunded= "Refunded";
         
